35fb76ae47f0c5fa3d21428c64648d59ba68df9a
Anri Tvalabeishvili <anri.tvalabeishvili1@gmail.com>
2024-09-29 11:07:59 +0400

    update readme file

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, find_packages
 
+# python setup.py sdist bdist_wheel
+# pip install dist/ScoringPy-0.0.2.tar.gz
+# pip install dist/ScoringPy-0.0.2-py3-none-any.whl
+# pip install --force-reinstall dist/ScoringPy-0.0.2-py3-none-any.whl
+# twine upload dist/*
+
+
+
 
 setup(
     name="AlertManager",  # The name of your package
-    version="0.0.1",  # The version of your package
-    author="Anri Tvalabeishvili, Kristine Dzneladze, Ruso Kvesitadze",  # Your name or organization
-    author_email="anri.tvalabeishvili@gmail.com",  # Your contact email
-    description="A job scheduling library that supports various time intervals",  # A short description of your package
+    version="0.0.3",  # The version of your package
+    author="Anri Tvalabeishvili, Kristine Dzneladze",  # Your name or organization
+    author_email="QubdiSolutions@gmail.com",  # Your contact email
+    description="AlertManager is an open-source Python library designed to streamline and enhance data validation processes for both local Pandas DataFrames and database tables.",  # A short description of your package
     long_description=open("README.md").read(),  # Long description read from a file
     long_description_content_type="text/markdown",  # The format of the long description (usually 'text/markdown')
-    url="https://github.com/DataShelf-Solutions/SchedulePy",  # URL to your project's homepage (GitHub, etc.)
+    url="https://github.com/Qubdi/AlertManager",  # URL to your project's homepage (GitHub, etc.)
     packages=find_packages(),  # Automatically find and include all packages in the project
     install_requires=[
         # List your package dependencies here
@@ -17,8 +25,8 @@ setup(
     ],
     project_urls={
         'Documentation': 'https://timeline-manager.readthedocs.io/en/latest/index.html',
-        'HomePage': 'https://github.com/DataShelf-Solutions/SchedulePy',
-        'Tracker': 'https://github.com/DataShelf-Solutions/SchedulePy/issues',
+        'HomePage': 'https://github.com/Qubdi/AlertManager',
+        'Tracker': 'https://github.com/Qubdi/AlertManager/issues',
     },
     classifiers=[
         "Programming Language :: Python :: 3",  # Specify the Python versions supported
